0eba20226657678e9af4bf05b8833dde42e80da6
Jason Kien <kien.jason@gmail.com>
2015-10-21 01:36:34 -0400

    mostly thru part 5

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,8 +4,15 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 	
 	public GameObject CurrentCheckPoint;
-	
+
 	private PlayerController Player;
+
+	//drag object in the inspector
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	//used to delay time between death and respawn
+	public float RespawnDelay;
 	
 	// Use this for initialization
 	void Start () {
@@ -18,7 +25,32 @@ public class LevelManager : MonoBehaviour {
 	}
 	
 	public void RespawnPlayer() {
+
+		StartCoroutine("RespawnPlayerCo");
+	}
+
+	//a coroutine runs on it's own timeloop
+	public IEnumerator RespawnPlayerCo()
+	{
+		//create game object need position and rotation
+		Instantiate(DeathParticle, Player.transform.position, Player.transform.rotation);
+
+		//disable player, so he can't move 
+		Player.enabled = false;
+
+		//hide the player
+		Player.GetComponent<Renderer>().enabled = false;
+
 		Debug.Log("Player Respawn");
+
+		//pause
+		yield return new WaitForSeconds(RespawnDelay);
+
 		Player.transform.position = CurrentCheckPoint.transform.position;
+
+		Player.enabled = true;
+		Player.GetComponent<Renderer>().enabled = true;
+		
+		Instantiate(RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
 	}
 }
