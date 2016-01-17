c75b9005974ff6e2591b35ad3ef7da96a157285f
jkien <JasonKien@Jasons-MacBook-Pro.local>
2016-01-17 16:56:53 -0500

    mid part 13

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	private bool playerInZone;
+
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		playerInZone = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 		if (Input.GetKeyDown (KeyCode.W) && playerInZone) 
+		{
+			//can also use loadlevelasync if we wanted some animation or music while level loading
+			Application.LoadLevel(levelToLoad);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			playerInZone = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			playerInZone = false;
+		}
+	}
+	
+}
